13036489a606131481a8d48d60fbf71379fe1550
J <samsung@jameson.cc>
2016-12-02 00:29:17 -0500

    test

--- smartapps/sc/http-button-creator.src/http-button-creator.groovy
+++ smartapps/sc/http-button-creator.src/http-button-creator.groovy
@@ -14,7 +14,7 @@
  *
  *  Credit: Eric Roberts (baldeagle072) - Virtual switch creator
  *  Credit: tguerena and surge919 - URI Switch
- *
+ *  
  *  Fix addChildDevice problem - https://community.smartthings.com/t/use-addchilddevice-with-manual-ip-entry/4594/23
  */
 definition(
@@ -29,6 +29,9 @@ definition(
 
 
 preferences {
+	section("Select Sensor") {
+       input("sensorDevice","capability.contactSensor", title: "pick a contact sensor", required: true, multiple: false)
+    }
 	section("Create HTTP Button") {
 		input "switchLabel", "text", title: "Button Label", required: true
 	}
@@ -41,6 +44,11 @@ def installed() {
 	log.debug "Installed with settings: ${settings}"
 
 	initialize()
+    
+    subscribe(sensorDevice, "contact", contactHandler)
+    state.currentState = "sigh"
+    
+    log.debug "Installed current state; ${state.currentState}"
 }
 
 def updated() {
@@ -55,9 +63,13 @@ def initialize() {
     log.debug(deviceId)
     def existing = getChildDevice(deviceId)
     if (!existing) {
-        def childDevice = addChildDevice("sc", "HTTP Switch", deviceId, null, [label: switchLabel])
-        //def childDevice = addChildDevice("sc", "HTTP Button", deviceId, theHub.id, [label: switchLabel])
+        def childDevice = addChildDevice("sc", "HTTP Switch", deviceId, theHub.id, [label: switchLabel, completedSetup: true])
     }
+    
+     getChildDevices().each {
+         subscribeToCommand(it, "on", onCommand)
+         subscribeToCommand(it, "off", onCommand)
+ 	 }
 }
 
 def uninstalled() {
@@ -68,4 +80,60 @@ private removeChildDevices(delete) {
     delete.each {
         deleteChildDevice(it.deviceNetworkId)
     }
+}
+
+def contactHandler(evt) {
+   def childDevice =  getChildDevice(app.id)
+  
+  if("open" == evt.value) {
+    log.debug "Contact is in ${evt.value} state" 
+   	sendEvent(childDevice,[name:"switch", value:"on", data: [type:"manual"]])
+  }
+  
+  if("closed" == evt.value) {
+    log.debug "Contact is in ${evt.value} state"
+    sendEvent(childDevice,[name:"switch", value:"off", data: [type:"manual"]])
+  }
+}
+
+def onCommand(evt) {
+	def currentValue = "${sensorDevice.currentValue('contact').value}"
+    def isclosed = currentValue == 'closed'
+	log.debug "Calling on ${evt.value} ${currentValue} "
+    
+    if (evt.data == null) { //real command, not being set by the door itself
+    	def childDevice =  getChildDevice(app.id)
+        log.debug "Current: ${currentValue} ${isclosed}, Val: ${evt.value == 'on'}"
+        def needToToggle = (currentValue == 'closed' && evt.value == 'on') || (currentValue == 'open' && evt.value == 'off')
+        
+        if (needToToggle)
+   			sendHubCommand(runCmd())
+        else 
+            log.debug 'DO NOT NEED TO TOGGLE'
+    }
+}
+
+def runCmd() {
+	def host = '192.168.1.80'
+	def LocalDevicePort = '80'
+	def path = '/toggle_door'
+	def body = "" 
+
+	def headers = [:] 
+	headers.put("HOST", "$host:$LocalDevicePort")
+
+	def method = "GET"
+	try {
+		def hubAction = new physicalgraph.device.HubAction(
+			method: method,
+			path: path,
+			body: body,
+			headers: headers
+			)
+		log.debug hubAction
+		return hubAction
+	}
+	catch (Exception e) {
+		log.debug "Hit Exception $e on $hubAction"
+	}
 }
